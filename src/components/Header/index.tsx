99347a31857cb84ced9862179fdf4a66d019eb75
codemoises <88902469+codemoises@users.noreply.github.com>
2024-04-16 18:02:51 -0300

    Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,38 +1,12 @@
-import Button from '../Button';
+import MobileNavBar from '../MobileNavBar';
+import NavBar from '../NavBar';
 import * as Styled from './styles';
 
 export default function Header() {
   return(
     <Styled.Header>
-      <Styled.NavBar>
-        <Styled.Logo>
-          <Styled.H1>
-            teslabank.
-          </Styled.H1>
-        </Styled.Logo>
-        <Styled.Nav>
-          <Styled.Ul>
-            <Styled.Li>
-              <Styled.Link>
-                Benefícios
-              </Styled.Link>
-              <Styled.Link>
-                Conta digital
-              </Styled.Link>
-              <Styled.Link>
-                Suporte
-              </Styled.Link>
-              <Styled.Link>
-                Blog
-              </Styled.Link>
-            </Styled.Li>
-          </Styled.Ul>
-        </Styled.Nav>
-        <Styled.Div>
-          <Button children="Entrar" backgroundColor={false} textColor={true} />
-          <Button children="Abrir conta" backgroundColor textColor={false} />
-        </Styled.Div>
-      </Styled.NavBar>
+      <NavBar />
+      <MobileNavBar />
     </Styled.Header>
   );
 };
