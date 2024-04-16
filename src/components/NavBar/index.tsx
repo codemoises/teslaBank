b541b6b912b3cdfb98d619ff3e826cc7971dc0ed
codemoises <88902469+codemoises@users.noreply.github.com>
2024-04-16 18:02:04 -0300

    NavBar desktop

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,39 @@
+import Button from '../Button';
+import Logo from '../Logo';
+import * as Styled from './styles';
+
+export default function NavBar() {
+  return(
+    <Styled.NavBar>
+        <Logo />
+        <Styled.Nav>
+          <Styled.Ul>
+            <Styled.Li>
+              <Styled.Link>
+                Benefícios
+              </Styled.Link>
+            </Styled.Li>
+            <Styled.Li>
+              <Styled.Link>
+                Conta digital
+              </Styled.Link>
+            </Styled.Li>
+            <Styled.Li>
+              <Styled.Link>
+                Suporte
+              </Styled.Link>
+            </Styled.Li>
+            <Styled.Li>
+              <Styled.Link>
+                Blog
+              </Styled.Link>
+            </Styled.Li>
+          </Styled.Ul>
+        </Styled.Nav>
+        <Styled.Div>
+          <Button children="Entrar" backgroundColor={false} textColor={true} />
+          <Button children="Abrir conta" backgroundColor textColor={false} hover borderRadius />
+        </Styled.Div>
+      </Styled.NavBar>
+  );
+}
